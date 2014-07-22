b3a0c870b9aa8e1b44a62c2ebe5b7c243be83cc4
Wissem BOUSSETTA <boussetta.wissem@hotmail.fr>
2014-07-22 16:18:27 +0100

    New Examples: Combined Parallel Loop Reduction Environment Information Matrix Multiply Parallel Region Orphaned Directive
    Config file for sample selection

---
+++ Config.h
@@ -0,0 +1,19 @@
+/*
+ * Config.h
+ *
+ *  Created on: Jul 22, 2014
+ *      Author: Wissem BOUSSETTA
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define HELLO_OPEN_MP						0
+#define COMBINED_PARALLEL_LOOP_REDUCTION	0
+#define ENVIRONMENT_INFORMATION				0
+#define LOOP_WORK_SHARING					0
+#define MATRIX_MULTIPLY						1
+#define PARALLEL_REGION_ORPHANED_DIRECTIVE	0
+#define SECTION_WORK_SHARING				0
+
+#endif /* CONFIG_H_ */
