97989a7c9237981c4b6e3539480dc8fa4ef69305
Wissem BOUSSETTA <boussetta.wissem@hotmail.fr>
2014-07-30 05:28:04 +0200

    CFLAG for debug issue fixed.

--- CombinedParallelLoopReduction.cpp
+++ CombinedParallelLoopReduction.cpp
@@ -21,12 +21,12 @@ int main(int argc, char *argv[]) {
 	/* Some initializations */
 	n = 100;
 	for (i = 0; i < n; i++)
-	a[i] = b[i] = i * 1.0;
+		a[i] = b[i] = i * 1.0;
 	sum = 0.0;
 
 #pragma omp parallel for reduction(+:sum)
 	for (i = 0; i < n; i++)
-	sum = sum + (a[i] * b[i]);
+		sum = sum + (a[i] * b[i]);
 
 	printf("   Sum = %f\n", sum);
 
